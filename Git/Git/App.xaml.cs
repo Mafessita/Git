237605daceb734331e4fc0e243f688f755dfe760
Mafessita <maria.garciaco@cecar.edu.co>
2024-10-15 07:13:12 -0500

    Proyecto completo

---
+++ Git/Git/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Git
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
